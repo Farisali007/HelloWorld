59f95ec3fcaeabab5848d2b4deb2f0ef57d8c53e
unknown <you@example.com>
2019-05-21 01:47:24 +0500

    a

---
+++ src/Basic.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View } from 'react-native';
+import pic from './pic';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+   export default class App extends Component{
+     myfunction(){
+
+     }
+   
+
+   render(){
+    
+   return(
+      <View style={styles.container}>
+      <Text style={styles.Welcome}>
+      This is my first App
+      </Text>
+      </View>
+
+    );
+   }
+  }
+
+   const header = () =>
+   (
+     <View>
+       <Text> header</Text>
+     </View>
+   )
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  
+Images: {
+  width: 93,
+  height: 10,
+
+},
+
+});
+export default Basic;
